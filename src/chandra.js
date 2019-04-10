d6b957f1c3ec27e36662e1bfdb236d5ed0ecd8c8
tsgouros <tsgouros@gmail.com>
2019-04-09 20:56:52 -0400

    trying something with the glb mesh

--- src/chandra.js
+++ src/chandra.js
@@ -1,3 +1,37 @@
-document.getElementById("body").addEventListener("load", function() {
-  console.log("loading body...");
-});
+
+(function(modelEl) {
+  if (!window['AFRAME'] && !modelEl) {
+    return;
+  }
+
+  modelEl.addEventListener('model-loaded', function(evt) {
+    var model = evt.detail.model;
+
+    traverse(model);
+  });
+})(document.getElementById('chandra'));
+
+function traverse(node) {
+  node.children.forEach(function(child) {
+    if (child.children) {
+      traverse(child);
+    }
+
+    updateMaterial(child['material'], THREE.DoubleSide);
+  });
+}
+
+function updateMaterialSide(material, side) {
+  if (!material) {
+    return;
+  }
+
+  if (material instanceof THREE.Material) {
+    material.side = side;
+    material.needsUpdate = true
+  } else if (material instanceof THREE.MultiMaterial) {
+    material.materials.forEach(function(childMaterial) {
+      updateMaterial(childMaterial, side);
+    });
+  }
+}
